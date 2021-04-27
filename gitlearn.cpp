7180dcdf1e9c9e2452022d992e29036cb02aa56e
xdy <bitxdy@163.com>
2021-04-27 19:56:18 +0800

    zuihou

--- gitlearn.cpp
+++ gitlearn.cpp
@@ -3,6 +3,6 @@ int main(){
     std::cout<<"hello world"<<endl;
     std::cout<<"hello git"<<endl;
     std::cout<<"hello you"<<endl;
-    std::cout<<"git tracks changes"<<endl;
+    std::cout<<"git tracks changes of files"<<endl;
     return 0;
 }
